e4982f1c41e1e47543dea06b90278032cf44183c
Francisco <francixcoag@hotmail.com>
2016-04-04 04:35:37 +0100

    minor fix

--- prototipo/js/searchScript.js
+++ prototipo/js/searchScript.js
@@ -10,12 +10,7 @@ $(document).ready(function() {
 			'max-height': '100%',
 			'max-width': '100%'
 		});
-		$('#keyboard').animate(
-			{ opacity: 1 },
-			{
-				duration: 'slow',
-				easing: 'easeOutBounce'
-			});
+		
 		return false;
 	
 	});
