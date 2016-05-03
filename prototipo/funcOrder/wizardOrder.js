3cfa2a4a3ac51361bd8f9a863b1621e4a4e35a75
Fernando Costa Oliveira <ist166983@lab1p1.rnl.tecnico.ulisboa.pt>
2016-05-03 12:31:36 +0100

    after merge, adding new stuff. just making a break

--- prototipo/funcOrder/wizardOrder.js
+++ prototipo/funcOrder/wizardOrder.js
@@ -19,6 +19,8 @@ function clearPagamento() {
     	document.body.removeChild(curpopup);
 };
 
+var trickrelogio;
+
 function pagaOqDeves() {	
     var xhttp = new XMLHttpRequest();
     var want = "funcOrder/pagar.html";
@@ -32,5 +34,44 @@ function pagaOqDeves() {
     xhttp.open("GET", want, true);
     xhttp.send();
     
+    trickrelogio = setTimeout(populatePayment, 5);
+    
+};
+
+function populatePayment() {
+    var coisas = "";
+    
+    if (funcComida.shopCart.length != 0) {
+        coisas = "<li><input type='checkbox' id='pedido0'>";
+        
+        coisas += "<a>";
+        coisas += "#Pedido currente - ";
+        coisas += funcComida.shopCartTotal;
+        coisas += "€";
+        coisas += "</a> ";
+        coisas += "</li>";
+    }
+    for (i = 0; i < funcOrder.orderLog.length; i++) {
+        coisas += "<li> <input type='checkbox' id='";
+        coisas += "pedido"+(i+1);
+        coisas += "'><a>";        
+        coisas += "#"+(i+1)+" pedido - ";
+        coisas += funcOrder.orderLog[i][1];
+        coisas += "€";
+        coisas += "</a>";
+        
+        coisas += " </li>";
+    }
+    
+    document.getElementById("pedidosLista").innerHTML = coisas;
+};
+
+function showChecks() {
+    var foo = document.querySelectorAll("input");    
+    for(i = 0; i < foo.length; i++) {        
+        if (foo[i].style.display == "none") {
+            foo[i].style.display = "inline";
+        } else foo[i].style.display = "none";
+    }
     
 };
